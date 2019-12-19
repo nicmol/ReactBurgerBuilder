59b0ec1819ad11f99bfc227bbfe1322cfc4000d0
nmolitor <nmolitor@uoregon.edu>
2019-12-18 16:01:34 -0800

    Drawer Toggle not working

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -13,9 +13,15 @@ class Layout extends Component {
     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
     }
+
+    sideDrawerTogglehandler = () => {
+        this.setState((prevState) => 
+        {return {showSideDrawer: !prevState.showSideDrawer};
+    });
+}
     render(){
         return(<Aux>
-            <Toolbar />
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
             <SideDrawer 
                 open={this.state.showSideDrawer} 
                 closed ={this.sideDrawerClosedHandler} />
